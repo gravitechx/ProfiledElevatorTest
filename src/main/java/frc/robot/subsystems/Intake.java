226ee0c4016235e30aa149ed002f080a1050f830
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-11-10 23:13:13 -0800

    Create new intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.SlotConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
+    private TalonFX intakeMotor = new TalonFX(0);
+    public Intake(){
+        this.intakeMotor.getConfigurator().apply(
+            new MotorOutputConfigs()
+                .withNeutralMode(NeutralModeValue.Brake)
+                .withInverted(InvertedValue.Clockwise_Positive)
+        );
+    }
+
+    public void setOutput(double output){
+        this.intakeMotor.set(output);
+    }
+
+    public double getEncoderRate(){
+        return this.intakeMotor.getVelocity().getValueAsDouble();
+    }
+}
